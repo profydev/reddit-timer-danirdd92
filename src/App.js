1ff531e59153e558d21152ef86df707fbbdf9151
Daniel Rashba <danirdd92@gmail.com>
2022-04-06 23:57:58 +0300

    Create app skeleton (#1)
    * created app skeleton
    
    * fixed router preventing test from succeeding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element="" />
+          <Route path="search" element="" />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
